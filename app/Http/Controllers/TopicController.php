7fc1d093100d30899c98ef8d27c8ff377544ed38
alvaro220592 <alvaro220592@gmail.com>
2022-10-18 21:17:46 -0300

    first commit

---
+++ app/Http/Controllers/TopicController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topic;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class TopicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Inertia::render('Topics/Index', [
+            'topics' => Topic::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Topics/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Topic $topic)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Topics/Create.vue
@@ -0,0 +1,39 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, Link } from "@inertiajs/inertia-vue3";
+
+</script>
+
+<template>
+    <Head title="Topics" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Topics
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-1 bg-white border-b border-gray-200">
+                       
+
+                        <!-- component -->
+                        <section class="container mx-auto p-6 font-mono">
+                        <div class="flex p-2 m-2">
+                            <Link href="/topics" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded">Back</Link>
+                        </div>
+                            <div
+                                class="w-full mb-8 overflow-hidden rounded-lg shadow-lg"
+                            >
+                            
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
